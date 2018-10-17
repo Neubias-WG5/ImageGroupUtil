d1df696268d722c8d591ca30a52e4c5320cd1a2b
Benjamin Pavie <benjamin.pavie@gmail.com>
2018-10-17 14:44:06 +0200

    Switch from class to function

--- image_group_util/__init__.py
+++ image_group_util/__init__.py
@@ -1,10 +1,11 @@
 # from .mask_to_objects import AnnotationSlice, mask_to_objects_2d, mask_to_objects_3d, mask_to_objects_3dt
 # from .mask_to_points import mask_to_points_2d
-from image_group_util.image_group_download import ImageGroupDownloader
+from .image_group_download import download_attach_files_from_image_group, download_all_image_group_from_project,\
+    download_image_group
 
 
 __version__ = "0.0.1"
 
 __all__ = [
-    "ImageGroupDownloader"
+    "download_attach_files_from_image_group","download_all_image_group_from_project","download_image_group"
 ]
